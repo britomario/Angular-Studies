51fb3dd1755e1c2fbc089ff0a8b0b6ec7cd966e1
Mario Brito <79857158+britomario@users.noreply.github.com>
2023-09-14 17:25:00 -0300

    my first commit with angular

---
+++ meu-primeiro-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-title title="Olá mundo!!!"></app-title>
+  <router-outlet></router-outlet>`
+})
+export class AppComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Hello')
+    }, 5000)
+  }
+}
---
+++ meu-primeiro-app/src/app/title/title.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit, OnChanges {
+
+  @Input() public title:string = "Bem vindo!";
+
+  constructor () {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+      alert('Foi alterado com sucesso!!!')
+  }
+}
